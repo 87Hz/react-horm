04671dc7be49d58dd14e6fd4a7e5bbee63fada75
HU Ze <huze@outlook.sg>
2019-05-20 22:50:47 +1200

    useEffect working

--- src/main/demo/app.cljs
+++ src/main/demo/app.cljs
@@ -1,6 +1,6 @@
 (ns demo.app
   (:require ["react-dom" :refer (render)]
-            ["react" :refer (useState createElement)]
+            ["react" :refer (useState createContext useContext useEffect) :rename {createElement e}]
             ; ["./bar" :refer (MyComponent) :default MyDefaultComponent]
             [horm :as horm]))
 
@@ -11,15 +11,24 @@
 ;                                      (range num-children)))]
 ;     (MyDefaultComponent #js {:children (mapv MyComponent children-props)})))
 
+(def ctx (createContext 0))
+
 (defn stateExample []
-  (let [[val set-val!] (useState 200)]
-    (createElement "div" nil
-                   (createElement "button" #js {:onClick (fn [] (set-val! inc))} "+")
-                   (createElement "button" #js {:onClick (fn [] (set-val! dec))} "-")
-                   val)))
+  (let [ctx-val (useContext ctx)
+        [val set-val!] (useState ctx-val)]
+    (useEffect
+      (fn []
+        (prn 123)
+        js/undefined)
+      [])
+    (e "div" nil
+      (e "button" #js {:onClick (fn [] (set-val! inc))} "+")
+      (e "button" #js {:onClick (fn [] (set-val! dec))} "-")
+      val)))
 
 (defn demo []
-  (createElement stateExample))
+  (e ctx.Provider #js {:value 100}
+    (e stateExample)))
 
 (defn ^:export mount []
   (render (demo) (.getElementById js/document "app")))
